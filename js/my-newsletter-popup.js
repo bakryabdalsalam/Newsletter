7c9d537a6bb61e432b801ebd64e2d82addae7afb
Bakry Abdalsalam <67397650+bakryabdalsalam@users.noreply.github.com>
2024-12-09 14:57:52 +0000

    fix the form

--- js/my-newsletter-popup.js
+++ js/my-newsletter-popup.js
@@ -2,11 +2,21 @@ jQuery(document).ready(function($) {
     if (!localStorage.getItem('newsletterPopupShown')) {
         localStorage.setItem('newsletterPopupShown', 'true');
 
-        // Create popup overlay
-        $('body').append('<div id="newsletter-popup-overlay"></div>');
-
-        // Display the form in the popup
-        $('#newsletter-popup-overlay').append($('#my-newsletter-form').parent().html());
+        // Create popup overlay with form
+        var popupHtml = `
+            <div id="newsletter-popup-overlay">
+                <div id="newsletter-popup-content">
+                    <form id="my-newsletter-form">
+                        <!-- Your form fields -->
+                        <label for="my_newsletter_subscribe" style="margin-bottom:10px;">
+                            <input type="checkbox" name="my_newsletter_subscribe" id="my_newsletter_subscribe" value="1"> Subscribe to Newsletter
+                        </label><br><br>
+                        <button type="submit" class="form-button" style="width:100%;">Send</button>
+                    </form>
+                </div>
+            </div>
+        `;
+        $('body').append(popupHtml);
 
         // Close popup on overlay click
         $('#newsletter-popup-overlay').on('click', function(e) {
